10e449cc80b20ab6ba1846314873c2b553c666cc
leeallen1986 <lee.allen@atlascopco.com>
2026-07-19 19:56:39 +0800

    feat(fp): add Next Best 5 to This Week

--- client/src/pages/ThisWeekWithFullPotential.tsx
+++ client/src/pages/ThisWeekWithFullPotential.tsx
@@ -1,10 +1,12 @@
 import FullPotentialMyWeekDock from "@/components/FullPotentialMyWeekDock";
+import FullPotentialNextBest5 from "@/components/FullPotentialNextBest5";
 import ThisWeek from "./ThisWeek";
 
 export default function ThisWeekWithFullPotential() {
   return (
     <>
       <ThisWeek />
+      <FullPotentialNextBest5 />
       <FullPotentialMyWeekDock />
     </>
   );
